e0e7f5e77df19b5f98fc9aa193298a097a0699f8
Erick Aguilar <ericksvaguilar@gmail.com>
2021-07-02 18:52:07 -0300

    feat(calculator): add class and import keyboard component

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,7 +1,12 @@
 import Display from './Display.js';
+import Keyboard from './Keyboard.js';
 
 export default () => {
 	const Calculator = document.createElement('div');
+
+	Calculator.classList.add('calculator');
+
 	Calculator.append(Display());
+	Calculator.append(Keyboard());
 	return Calculator;
 };
