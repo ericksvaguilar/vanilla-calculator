2143cec1b48f4f72316fdbad752be2c576e08f6f
Erick Aguilar <ericksvaguilar@gmail.com>
2021-07-02 18:21:21 -0300

    feat: append calculator to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Calculator from './Calculator/Calculator.js';
+
 const App = () => {
 	const App = document.createElement('div');
+
 	App.setAttribute('class', 'App');
+	App.append(Calculator());
+
 	return App;
 };
 
