7e2017404ce6986537f0c33e1a189f91bb64d43d
Erick Aguilar <ericksvaguilar@gmail.com>
2021-07-02 18:52:31 -0300

    feat(digit): add class and type attr

--- src/Calculator/Digit.js
+++ src/Calculator/Digit.js
@@ -1,5 +1,7 @@
-export default (text) => {
+export default (text, type = 'operand') => {
 	const Digit = document.createElement('span');
+
+	Digit.classList.add('digit', `digit-${type}`);
 	Digit.innerText = text;
 
 	return Digit;
