e780694c5f8240345db357ea82f035138cfbcd5c
Erick Aguilar <ericksvaguilar@gmail.com>
2021-07-03 08:20:44 -0300

    feat: finish data buttons

--- src/data/buttons.js
+++ src/data/buttons.js
@@ -1,10 +1,73 @@
 export const buttons = [
 	{
 		type: 'operand',
+		value: '7',
+	},
+	{
+		type: 'operand',
+		value: '8',
+	},
+	{
+		type: 'operand',
+		value: '9',
+	},
+	{
+		type: 'operator',
+		value: '/',
+	},
+	{
+		type: 'operand',
+		value: '4',
+	},
+	{
+		type: 'operand',
+		value: '5',
+	},
+	{
+		type: 'operand',
+		value: '6',
+	},
+	{
+		type: 'operator',
+		value: '*',
+	},
+	{
+		type: 'operand',
 		value: '1',
 	},
 	{
 		type: 'operand',
 		value: '2',
+	},
+	{
+		type: 'operand',
+		value: '3',
+	},
+	{
+		type: 'operator',
+		value: '-',
+	},
+	{
+		type: 'operand',
+		value: '0',
+		span: '2',
+	},
+	{
+		type: 'operand',
+		value: ',',
+	},
+	{
+		type: 'operator',
+		value: '+',
+	},
+	{
+		type: 'clear',
+		value: 'c',
+		span: '2',
+	},
+	{
+		type: 'equal',
+		value: '=',
+		span: '2',
 	},
 ];
