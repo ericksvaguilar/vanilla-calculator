f585e15e8c3c3d687d3e97aba12ff9a5c5cf2e28
Erick Aguilar <ericksvaguilar@gmail.com>
2021-07-03 08:19:42 -0300

    feat(button): add span prop

--- src/Calculator/Button.js
+++ src/Calculator/Button.js
@@ -1,7 +1,8 @@
-export default ({ type, value }) => {
+export default ({ type, value, span }) => {
 	const Button = document.createElement('button');
 
 	Button.classList.add('button', `button-${type}`);
+	span && Button.classList.add(`button-span-${span}`);
 	Button.innerText = value;
 
 	return Button;
