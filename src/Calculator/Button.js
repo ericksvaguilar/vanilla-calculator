93a394c637bf22ec0094c5556e6edc135b72ef8d
Erick Aguilar <ericksvaguilar@gmail.com>
2021-07-03 18:25:46 -0300

    feat: add button onclick event

--- src/Calculator/Button.js
+++ src/Calculator/Button.js
@@ -1,9 +1,12 @@
-export default ({ type, value, span }) => {
+export default (props) => {
+	const { type, value, span, onClickButton } = props;
+
 	const Button = document.createElement('button');
 
 	Button.classList.add('button', `button-${type}`);
 	span && Button.classList.add(`button-span-${span}`);
 	Button.innerText = value;
+	Button.addEventListener('click', onClickButton);
 
 	return Button;
 };
--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,5 +1,8 @@
 import Display from './Display.js';
 import Keyboard from './Keyboard.js';
+const onClickButton = (event) => {
+	console.log(event.target.innerText);
+};
 
 export default () => {
 	const Calculator = document.createElement('div');
@@ -7,6 +10,6 @@ export default () => {
 	Calculator.classList.add('calculator');
 
 	Calculator.append(Display());
-	Calculator.append(Keyboard());
+	Calculator.append(Keyboard(onClickButton));
 	return Calculator;
 };
