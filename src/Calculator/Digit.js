6279906e38566813d01a216739050cf946e3a973
Erick Aguilar <ericksvaguilar@gmail.com>
2021-07-04 12:51:19 -0300

    change component pattern to observer parrern

--- src/Calculator/Digit.js
+++
@@ -1,8 +0,0 @@
-export default (text, type = 'operand') => {
-	const Digit = document.createElement('span');
-
-	Digit.classList.add('digit', `digit-${type}`);
-	Digit.innerText = text;
-
-	return Digit;
-};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import App from './App.js';
+import createCalculator from './calculator.js';
+import createClickListener from './click-listener.js';
 
-document.body.append(App());
+const calculator = createCalculator();
+const clickListener = createClickListener(document);
+
+clickListener.subscribe((clickedElement) => {
+	const isOperandClick = clickedElement.classList.contains('button-operand');
+
+	if (isOperandClick) {
+		calculator.appendNumbers(clickedElement.innerText);
+	}
+});
+
+clickListener.subscribe(() => {
+	const previousOperand = document.querySelector('.previous-operand');
+	const currentOperand = document.querySelector('.current-operand');
+
+	previousOperand.innerText = calculator.state.previousOperand;
+	currentOperand.innerText = calculator.state.currentOperand;
+});
